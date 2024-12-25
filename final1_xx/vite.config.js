4476b8120b7a099fd09d87b2e214619605e42d95
jimmyhua123 <49429785+jimmyhua123@users.noreply.github.com>
2024-12-25 21:39:29 +0800

    Update vite.config.js

--- final1_xx/vite.config.js
+++ final1_xx/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 如果是子路徑部署，例如 '/final1_xx/'，請對應修改
+  base: '/final1_xx/', // 如果是子路徑部署，例如 '/final1_xx/'，請對應修改
   build: {
     outDir: 'dist', // 確保構建輸出的目錄是 dist
   },
